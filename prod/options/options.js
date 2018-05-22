ad24b65e19a3f63dc98a41665c6cb63a242f5313
Tatamo <nsorder@gmail.com>
2018-05-22 19:36:19 +0900

    add option page

---
+++ prod/options/options.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const default_urls = {
+	fx: "https://Ryo-9399.github.io/mc_canvas/Outputs/CanvasMasao.js",
+	kani2: "https://Ryo-9399.github.io/mc_canvas/Outputs/MasaoKani2.js",
+	v28: "https://Ryo-9399.github.io/mc_canvas/Outputs/CanvasMasao_v28.js"
+};
+
+// save options to chrome.storage
+function saveOptions() {
+	const url_fx = document.getElementById("url_fx").value;
+	const url_kani2 = document.getElementById("url_kani2").value;
+	const url_v28 = document.getElementById("url_v28").value;
+	chrome.storage.sync.set({url_fx, url_kani2, url_v28}, () => {
+		// Update status to let user know options were saved.
+		const status = document.getElementById('status');
+		status.textContent = 'Options saved.';
+		setTimeout(function () {
+			status.textContent = '';
+		}, 750);
+	})
+}
+
+// restore options and fill input form
+function restoreOptions() {
+	chrome.storage.sync.get({url_fx: default_urls.fx, url_kani2: default_urls.kani2, url_v28: default_urls.v28}, (items) => {
+		document.getElementById("url_fx").value = items.url_fx;
+		document.getElementById("url_kani2").value = items.url_kani2;
+		document.getElementById("url_v28").value = items.url_v28;
+	});
+}
+
+// reset all options to default and save them
+function revertOptionsToDefault() {
+	document.getElementById("url_fx").value = default_urls.fx;
+	document.getElementById("url_kani2").value = default_urls.kani2;
+	document.getElementById("url_v28").value = default_urls.v28;
+	const status = document.getElementById('status');
+	status.textContent = 'default options loaded (not yet be saved).';
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.getElementById('save').addEventListener('click', saveOptions);
+document.getElementById('reset').addEventListener('click', revertOptionsToDefault);
